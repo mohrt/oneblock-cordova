46154b7c4a53bd3f15a9f1b4f383f879434951d2
Monte Ohrt <monte@ohrt.com>
2015-07-14 23:47:24 -0500

    initialize templates

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ngStorage'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -42,12 +42,12 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
-  .state('app.create', {
-    url: '/create',
+  .state('app.edit', {
+    url: '/edit',
     views: {
       'menuContent': {
-        templateUrl: 'templates/create.html',
-        controller: 'CreateCtrl'
+        templateUrl: 'templates/edit.html',
+        controller: 'EditCtrl'
       }
     }
   })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,13 +11,56 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('ScanCtrl', function($scope, $state) {
+.controller('ScanCtrl', function($scope, $state, $ionicHistory) {
+  $scope.ids = angular.fromJson(localStorage.getItem('ids')) || [];
+  $ionicHistory.nextViewOptions({
+    disableBack: true
+  });
   $scope.go = function ( path ) {
     $state.go( path );
   };
 })
 
-.controller('CreateCtrl', function($scope) {
+.controller('EditCtrl', function($scope, $state, $ionicHistory, $ionicLoading) {
+  $scope.id = [];
+  $scope.page_title = 'Generate ID';
+  $scope.button_title = 'Generate ID';
+  $scope.isNew = true;
+  $scope.ids = angular.fromJson(localStorage.getItem('ids')) || [];
+
+  $scope.save = function(id) {
+    if(id.password !== id.password2) {
+        alert('passwords do not match');
+        return;
+    }
+    if(!_.isUndefined(id.index)) {
+        // id exists
+        $scope.ids[id.index].title = id.title;
+    } else {
+        $ionicLoading.show({
+          content: 'Loading',
+          animation: 'fade-in',
+          showBackdrop: true,
+          maxWidth: 200,
+          showDelay: 0
+        });
+        // generating a new id
+        var key = Bitcoin.ECKey.makeRandom();
+        var bip38 = new Bip38();
+        var keyenc = bip38.encrypt(key.toWIF(), id.password, key.pub.getAddress().toString());
+        $scope.ids.push({
+            title: id.title,
+            key: keyenc
+        });
+        localStorage.setItem('ids', angular.toJson($scope.ids));
+        $ionicLoading.hide();
+        $ionicHistory.nextViewOptions({
+          disableBack: true
+        });
+        $state.go( 'app.scan' );
+    }
+  };
+
 })
 
 .controller('ImportCtrl', function($scope) {
